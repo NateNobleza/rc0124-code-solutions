edfa3cfa8c3b829810db99fac7162e4b72d1cb0d
Nate Nobleza <noblezanathan@gmail.com>
2024-03-18 18:49:37 +0000

    react-JSX complete

---
+++ react-jsx/my-app/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React JSX</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Total Button Clicked: {count}
+        </button>
+        <p>
+          Q1: JSX is a javascript extension that allows develops to write the
+          code for dynamic DOM creation in a syntax that is very similar to
+          standard HTML
+        </p>
+        <p>
+          Q:2 React uses JXsS as a syntax extension for JavaScript to write
+          React components
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
