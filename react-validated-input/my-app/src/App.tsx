46a4ed37eceb8b412432159fc57be5fdd5a4931a
Nate Nobleza <noblezanathan@gmail.com>
2024-03-22 23:24:10 +0000

    validated input

---
+++ react-validated-input/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import ValidatedInput from './ValidatedInput';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Validated Input Example</h1>
+      <ValidatedInput />
+    </div>
+  );
+}
+
+export default App;
