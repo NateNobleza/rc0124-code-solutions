1cdbbf09d4d7e24e9175984f0bdd7792a287a06e
Nate Nobleza <noblezanathan@gmail.com>
2024-04-11 05:38:41 +0000

    completed practice

---
+++ full-stack-practice/client/src/pages/Catalog.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { readCatalog, Product, toDollars } from '../lib';
+import './Catalog.css';
+
+export function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function loadCatalog() {
+      try {
+        const loadedProducts = await readCatalog();
+        setProducts(loadedProducts);
+      } catch (err) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    setIsLoading(true);
+    loadCatalog();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) {
+    return <div>Error Loading Catalog: {error.message || 'Unknown Error'}</div>;
+  }
+
+  return (
+    <div className="container">
+      <h1>Catalog</h1>
+      <hr />
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.productId} className="col-12 col-md-6 col-lg-4">
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+type ProductCardProps = {
+  product: Product;
+};
+
+function ProductCard({ product }: ProductCardProps) {
+  const { productId, name, price, imageUrl, shortDescription } = product;
+
+  return (
+    <Link
+      to={`details/${productId}`}
+      className="product text-dark card mb-4 shadow-sm text-decoration-none">
+      <img src={imageUrl} className="image card-img-top" alt={name} />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text text-secondary">{toDollars(price)}</p>
+        <p className="description card-text">{shortDescription}</p>
+      </div>
+    </Link>
+  );
+}
